42f5f5978dd977f27711d99d30039fa793f21d6b
Data Mob <data.infra@tv4.se>
2018-04-10 16:32:19 +0200

    Add poll bigquery job completion sensor

--- airflow/contrib/sensors/bigquery_sensor.py
+++ airflow/contrib/sensors/bigquery_sensor.py
@@ -16,6 +16,7 @@ import logging
 
 from airflow.operators.sensors import BaseSensorOperator
 from airflow.contrib.hooks.bigquery_hook import BigQueryHook
+from airflow.contrib.hooks.bigquery_hook import BigQueryBaseCursor
 from airflow.utils.decorators import apply_defaults
 
 
@@ -67,3 +68,73 @@ class BigQueryTableSensor(BaseSensorOperator):
             bigquery_conn_id=self.bigquery_conn_id,
             delegate_to=self.delegate_to)
         return hook.table_exists(self.project_id, self.dataset_id, self.table_id)
+
+class BigQueryJobCompleteSensor(BaseSensorOperator):
+    """
+    Checks for the completion status of a job in Google Bigquery.
+    """
+    template_fields = ('project_id', 'job_id')
+    ui_color = '#f0eee4'
+
+    @apply_defaults
+    def __init__(
+            self,
+            project_id,
+            job_id,
+            bigquery_conn_id='bigquery_default_conn',
+            delegate_to=None,
+            *args,
+            **kwargs):
+        """
+        Create a new BigQueryJobCompleteSensor.
+
+        :param project_id: The Google cloud project in which to look for the table. The connection supplied to the hook
+        must provide access to the specified project.
+        :type project_id: string
+        :param job_id: The Google Bigquery jobId to poll for completion.
+        :type job_id: string
+        :param bigquery_conn_id: The connection ID to use when connecting to Google BigQuery.
+        :type bigquery_conn_id: string
+        :param delegate_to: The account to impersonate, if any.
+            For this to work, the service account making the request must have domain-wide delegation enabled.
+        :type delegate_to: string
+        """
+        super(BigQueryJobCompleteSensor, self).__init__(*args, **kwargs)
+        self.project_id = project_id
+        self.job_id = job_id
+        self.bigquery_conn_id = bigquery_conn_id
+        self.delegate_to = delegate_to
+
+    def poke(self, context):
+        from apiclient.discovery import HttpError, build
+
+        hook = BigQueryHook(bigquery_conn_id=self.bigquery_conn_id,
+            delegate_to=self.delegate_to)
+        service = hook.get_service()
+
+        bqCursor = BigQueryBaseCursor(service, self.project_id)
+
+        logging.info('Polling jobID: %s', self.job_id)
+
+        jobs = service.jobs()
+
+        try:
+            job = jobs.get(projectId=self.project_id, jobId=self.job_id).execute()
+            if (job['status']['state'] == 'DONE'):
+                return True
+        except HttpError as err:
+            if err.resp.status in [500, 503]:
+                self.log.info(
+                    '%s: Retryable error while polling job with id %s',
+                    err.resp.status, self.job_id)
+            else:
+                raise Exception(
+                    'BigQuery job status check failed. Final error was: %s',
+                    err.resp.status)
+        return False
+
+        # ISSUE Not able to find poll_job_complete function
+        #
+        # result = bqCursor.poll_job_complete(job_id = jobId)
+        # logging.info(result)
+        # return result
